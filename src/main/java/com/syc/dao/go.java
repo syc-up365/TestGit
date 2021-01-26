10ef7c9b21598f5b806d01296340d7c683d5b68a
syc_glb <syc_glb@qq.com>
2021-01-26 11:11:20 +0800

    已解决冲突

--- src/main/java/com/syc/dao/go.java
+++ src/main/java/com/syc/dao/go.java
@@ -3,11 +3,8 @@ package com.syc.dao;
 public class go {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("go out right");
-=======
 		System.out.println("go out left");
->>>>>>> branch 'master' of https://github.com/syc-up365/TestGit.git
 	}
 
 }
