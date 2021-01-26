1a4f97e2128f2cb7448ef9b72a66ed076b0f590b
eclipse_user <eclipse_user@syc.com>
2021-01-26 15:28:53 +0800

    hot_fix2

--- src/main/java/com/syc/dao/go.java
+++ src/main/java/com/syc/dao/go.java
@@ -5,6 +5,7 @@ public class go {
 	public static void main(String[] args) {
 		System.out.println("go out right");
 		System.out.println("hot_fix...");
+		System.out.println("hot_fix2...");
 	}
 
 }
